efc5435f2b1c8aa382e8e10b549be8e9262ba152
Varun Maharaj <varun@DESKTOP-AER3FMQ>
2020-08-11 11:48:08 +0200

    project initialisation

---
+++ SignalRChatAPI/HubConfig/ChartHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRChatAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChatAPI.HubConfig
+{
+    public class ChartHub : Hub
+    {
+        public async Task BroadcastChartData(List<ChartModel> data) => await Clients.All.SendAsync("broadcastchartdata", data);
+    }
+
+}
